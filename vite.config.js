96a8f8924ff119ba0aaee49bd4ccac2b2ae00d98
Andrey9019 <1234andrey5678@gmail.com>
2025-01-09 15:14:00 +0200

    setting package

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  // base: "/nannies-app/",
   plugins: [react()],
+  base: "/nannies-app/",
 });
