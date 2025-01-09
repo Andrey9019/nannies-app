ed0cb1edfa45c64c6806fd13a8c76416b44c2b6d
Andrey9019 <1234andrey5678@gmail.com>
2025-01-09 18:43:25 +0200

    setting package

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/nannies-app/",
+  // eslint-disable-next-line no-undef
+  base: process.env.NODE_ENV === "production" ? "/nannies-app/" : "/",
 });
