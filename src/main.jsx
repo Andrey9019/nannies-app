a05d39200e17e562b748be2927445c952b664635
Andrey9019 <1234andrey5678@gmail.com>
2025-01-10 13:03:50 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import FavoritesProvider from "./components/Favorite/content/FavoritesContext";
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <FavoritesProvider>
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/nannies-app/",
+  // eslint-disable-next-line no-undef
+  base: process.env.NODE_ENV === "production" ? "/nannies-app/" : "/",
+  build: {
+    outDir: "dist",
+  },
 });
